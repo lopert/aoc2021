5d38e30cfc7af273c718f8f37d5f1490bb981313
Alex Bradley <alexandre.bradley@shopify.com>
2021-12-07 07:25:37 -0500

    part1 almost

--- day4/sol.rb
+++ day4/sol.rb
@@ -14,12 +14,6 @@ class Bingo
     @cards = input.map do |card|
       BingoCard.new(card)
     end
-
-    solve
-
-    binding.pry
-    
-    
   end
 
   def solve
@@ -28,11 +22,9 @@ class Bingo
         card.mark(called_number)
       end
 
-      break if @cards.map do |card|
-        binding.pry if card.winner?
-
-        card.winner?
-      end.include?(true)
+      @cards.map do |card|
+        return card.calculate_win(called_number) if card.winner?
+      end
     end
   end
 
@@ -60,46 +52,79 @@ class BingoCard
   end
 
   def winner?
-    check_rows
+    check_rows || check_columns #|| check_crosses
   end
+
+  def calculate_win(winning_number)
+    @card.flatten.select{ |num| !num.include?("M") }.map(&:to_i).sum * winning_number.to_i
+  end
+
+  private
 
   def check_rows
     @card.each do |row|
-      row.each do |number|
-        puts number
-        binding.pry if number.include?("22M")
-        return false unless number.include?("M")
-        # binding.pry
-      end
+      return true if check_sequence(row)
     end
-    binding.pry
+    return false
+  end
+
+  def check_sequence(seq)
+    seq.each do |number|
+      return false unless number.include?("M")
+    end
     return true
   end
 
   def check_columns
-
+    (0..@card.length-1).each do |index|
+      column = @card.map do |row|
+        row[index]
+      end
+      return true if check_sequence(column)
+    end
+    return false
   end
 
+  # lmao didn't notice diagonals don't count FML
+  # What kind of bingo IS THIS!?!
   def check_crosses
+    range = (0..@card.length-1)
 
+    top_left_to_bottom_right = []
+    range.each do |row_index|
+      range.each do |col_index|
+        top_left_to_bottom_right << @card[row_index][col_index] if row_index == col_index
+      end
+    end
+    return true if check_sequence(top_left_to_bottom_right)
+
+    top_right_to_bottom_left = []
+    range.each do |row_index|
+      range.each do |col_index|
+        top_right_to_bottom_left << @card[row_index][col_index] if row_index + col_index == range.max
+      end
+    end
+    return true if check_sequence(top_right_to_bottom_left)
+    
+    return false
   end
 
 end
 
 
-solver = Bingo.new("./example_input.txt")
-# puts "Part1: #{solver.solve("./input.txt")}"
+# solver = Bingo.new("./example_input.txt")
+# puts "Part1: #{solver.solve}"
 # solver = BinDiagV2.new()
 # puts "Part2: #{solver.solve("./input.txt")}"
 
-# describe BinDiag do
-#   it "should return 198 for part one" do
-#     object_under_test = BinDiag.new()
-#     assert_equal 198, object_under_test.solve("./example_input.txt")
-#   end
+describe Bingo do
+  it "should return 4512 for part one example" do
+    object_under_test = Bingo.new("./example_input.txt")
+    assert_equal 4512, object_under_test.solve
+  end
 
-#   it "should return 230 for part two" do
-#     object_under_test = BinDiagV2.new()
-#     assert_equal 230, object_under_test.solve("./example_input.txt")
-#   end
-# end
+  # it "should return 230 for part two" do
+  #   object_under_test = BinDiagV2.new()
+  #   assert_equal 230, object_under_test.solve("./example_input.txt")
+  # end
+end
